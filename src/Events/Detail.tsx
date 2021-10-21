d3cd2f6d18230305c6fb96c102370752b900db1c
Franziska Bath <franziska.bath@instafreight.com>
2021-10-21 18:23:36 +0200

    add detail styling

--- src/Events/Detail.tsx
+++ src/Events/Detail.tsx
@@ -4,6 +4,7 @@ import doApiCall from '../helper/api';
 import moment from 'moment';
 import Event from '../sharedTypes/eventType';
 import Employee from '../sharedTypes/employeeType';
+import {Row, Col, Container, Card, ListGroup, Button} from 'react-bootstrap';
 
 type EventDetailProps = {
     location: {
@@ -35,23 +36,52 @@ const Detail = (props: EventDetailProps) => {
 
     return (
         <>
-            <p>
-                {event.position?.name}
-                {moment(event.startsAt).format('DD.MM.YYYY HH:mm')}
-                {moment(event.endsAt).format('DD.MM.YYYY HH:mm')}
-                {event.employees && event.employees.map((employee) => (
-                        <>
-                           <span>
-                               {employee.firstName} {employee.lastName}
-                           </span>
-                            <img src={employee.image} alt={`${employee.firstName} ${employee.lastName} avatar`} />
-                        </>
-                    ))
-            }
-            </p>
-            <button type='button' onClick={redirectToList}>
-                Back to list
-            </button>
+            <Container className="mt-3">
+                <ListGroup variant="flush">
+                    <ListGroup.Item>
+                        <Button variant="primary" onClick={redirectToList}>
+                            Back to list
+                        </Button>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Card style={{ width: '19rem' }}>
+                            <Card.Body>
+                                <Card.Title>Event Details</Card.Title>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>
+                                        <b>Name: </b>
+                                        {event.position?.name}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <b>Starts at: </b>
+                                        {moment(event.startsAt).format('DD.MM.YYYY HH:mm')}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <b>Ends at: </b>
+                                        {moment(event.endsAt).format('DD.MM.YYYY HH:mm')}
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Card.Title>Employees</Card.Title>
+                        {event.employees &&
+                        <Row xs={2} md={4} className="g-4">
+                            {event.employees.length === 0 ? <p>No employees found</p> : event.employees.map((employee) => (
+                                <Col>
+                                    <Card>
+                                        <Card.Img variant="top" src={employee.image} alt={`${employee.firstName} ${employee.lastName} avatar`} loading="lazy" />
+                                        <Card.Body>
+                                            <Card.Text>{employee.firstName} {employee.lastName}</Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            ))}
+                        </Row>}
+                    </ListGroup.Item>
+                </ListGroup>
+            </Container>
         </>
     )
 };
